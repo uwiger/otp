545b61868bb70edc603dc551774af008aa8eb5b5
Ulf Wiger <ulf.wiger@klarna.com>
2013-11-20 18:21:44 +0100

    Make DCD dumps explicitly available
    Setting the mnesia env variable 'dump_disc_copies_at_startup' to 'false'
    will disable the DCL dump at table load.
    
    If 'dump_disc_copies_at_startup' = true (default), the same test
    will be performed as for normal dumps, i.e. using 'dc_dump_limit'.
    
    If a DCL dump is desired on-demand, use the function
    mnesia_controller:snapshot_dcd(Tables). Tables must be a list of
    tables that have a local disc_copy. Otherwise an error will be returned.
    Once the operation actually executes, any table that doesn't have a
    local disc_copy is ignored.
    
    Specifically, the dump_log worker record has been changed to allow
    an arity-0 fun in stead of the default log dump. This fun will be
    executed as if it were a normal log dump, and must return 'dumped'.
    This could also be used e.g. to insert a backup operation between
    log dumps.

--- lib/mnesia/src/mnesia_monitor.erl
+++ lib/mnesia/src/mnesia_monitor.erl
@@ -669,6 +669,7 @@ env() ->
      backup_module,
      debug,
      dir,
+     dump_disc_copies_at_startup,
      dump_log_load_regulation,
      dump_log_time_threshold,
      dump_log_update_in_place,
@@ -699,6 +700,8 @@ default_env(debug) ->
 default_env(dir) ->
     Name = lists:concat(["Mnesia.", node()]),
     filename:absname(Name);
+default_env(dump_disc_copies_at_startup) ->
+    true;
 default_env(dump_log_load_regulation) ->
     false;
 default_env(dump_log_time_threshold) ->
@@ -752,6 +755,7 @@ do_check_type(debug, trace) -> trace;
 do_check_type(debug, true) -> debug;
 do_check_type(debug, verbose) -> verbose;
 do_check_type(dir, V) -> filename:absname(V);
+do_check_type(dump_disc_copies_at_startup, B) -> bool(B);
 do_check_type(dump_log_load_regulation, B) -> bool(B);
 do_check_type(dump_log_time_threshold, I) when is_integer(I), I > 0 -> I;
 do_check_type(dump_log_update_in_place, B) -> bool(B);
